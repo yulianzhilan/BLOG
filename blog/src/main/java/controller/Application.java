c76a1cd17d22ec7c17546f5fa7816c1eb43a7f56
yulianzhilan <18516669265@163.com>
2017-03-13 16:56:47 +0800

    3/13

---
+++ blog/src/main/java/controller/Application.java
@@ -0,0 +1,19 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by scott on 2017/3/13.
+ */
+@Controller
+@RequestMapping("/login")
+public class Application {
+
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+    public ModelAndView execute(){
+        return new ModelAndView("index");
+    }
+}
